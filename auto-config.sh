7cfef67b5c0c0314eefd39a1f5785e5b2c32e8c6
Brian Rojas Chale <Brarojasc@gmail.com>
2019-10-17 01:15:25 -0600

    updating the params receive and adding credential helper in cache

--- auto-config.sh
+++ auto-config.sh
@@ -1,24 +1,23 @@
 #! /bin/bash
 
 # Control variables
-args=("$@")
-name=${args[0]}
-email=${args[1]}
+name=$1
+email=$2
 
-git config --global alias.co checkout;
-git config --global alias.ci commit;
-git config --global alias.st status;
-git config --global alias.br branch;
-git config --global alias.cim 'commit -m';
-git config --global alias.nb 'checkout -b';
-git config --global alias.db 'checkout -D';
-git config --global alias.pu 'push -u';
-git config --global alias.rh 'reset --hard';
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.cim 'commit -m'
+git config --global alias.nb 'checkout -b'
+git config --global alias.db 'checkout -D'
+git config --global alias.pu 'push -u'
+git config --global alias.rh 'reset --hard'
 git config --global alias.lola 'log --graph --decorate --pretty=oneline --abbrev-commit --all'
 git config --global alias.lol 'log --graph --decorate --pretty=oneline --abbrev-commit'
+git config --global credential.helper cache
 
-if [[ ! -z "$name" ]] && [[ ! -z "$email" ]]
-then
-    git config --global user.name "$name";
-    git config --global user.email "$email";
+if [ ! -z "$name" ] && [ ! -z "$email" ]; then
+    git config --global user.name "$name"
+    git config --global user.email "$email"
 fi
