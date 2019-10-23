24c1f27c657e90c9c2ac72efd8f41ce610eec8ad
Brian Rojas Chale <Brarojasc@gmail.com>
2019-10-22 23:36:27 -0600

    adding new lola

--- auto-config.sh
+++ auto-config.sh
@@ -13,7 +13,7 @@ git config --global alias.nb 'checkout -b'
 git config --global alias.db 'checkout -D'
 git config --global alias.pu 'push -u'
 git config --global alias.rh 'reset --hard'
-git config --global alias.lola 'log --graph --decorate --pretty=oneline --abbrev-commit --all'
+git config --global alias.lola "log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
 git config --global alias.lol 'log --graph --decorate --pretty=oneline --abbrev-commit'
 git config --global alias.rt 'rm -r --cached .'
 git config --global credential.helper cache
